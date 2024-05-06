2b1de3b1f867a6ddb8075c4b005ab2db5ebf16e8
a23alera <a23alera@student.his.se>
2024-05-07 00:45:30 +0200

    La till texten jag vill ska synas i about skärmen och även centrerade texten och gjorde texten större

--- app/src/main/java/com/example/project/AboutActivity.java
+++ app/src/main/java/com/example/project/AboutActivity.java
@@ -11,6 +11,6 @@ public class AboutActivity extends AppCompatActivity {
         setContentView(R.layout.aboutlayout);
 
         TextView textView = findViewById(R.id.about_text_view);
-        textView.setText("Text saker");
+        textView.setText("Denna app är gjord för sånna som är intresserade inom hockey och Alex hockey league. I appen kan man välja ett av dom fem hockey lagen och få ut detaljer om det som hur mycket det är värt och deras hemma stad");
     }
 }
