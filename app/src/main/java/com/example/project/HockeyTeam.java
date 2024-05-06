8bbb58f504f3319674c487e9f1d78f7cc853e5ad
a23alera <a23alera@student.his.se>
2024-05-06 17:53:53 +0200

    fixade till så allt från json datan skulle matcha i min HockeyTeam.java attributer

--- app/src/main/java/com/example/project/HockeyTeam.java
+++ app/src/main/java/com/example/project/HockeyTeam.java
@@ -2,21 +2,63 @@ package com.example.project;
 
 public class HockeyTeam {
     private String id;
+    private String login;
     private String name;
     private String location;
     private int size;
     private int cost;
-    private String wiki;
-    private String imgUrl;
+    private String category;
+    private String auxData;
+    private String company;
 
-    public HockeyTeam(String id, String name, String type, String location, int size, int cost, String wiki, String imgUrl) {
+    // Uppdaterad konstruktor
+    public HockeyTeam(String id, String login, String name, String location, int size, int cost, String category, String auxData, String company) {
         this.id = id;
+        this.login = login;
         this.name = name;
         this.location = location;
         this.size = size;
         this.cost = cost;
-        this.wiki = wiki;
-        this.imgUrl = imgUrl;
+        this.category = category;
+        this.auxData = auxData;
+        this.company = company;
+    }
+
+    // Getter-metoder
+    public String getId() {
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getAuxData() {
+        return auxData;
+    }
+
+    public String getCompany() {
+        return company;
     }
 
     @Override
